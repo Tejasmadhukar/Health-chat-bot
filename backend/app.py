44fad406696f34a9cd49a5c1835d2884286b3cc7
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-15 16:36:10 +0530

    Removed deprecated functions

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 from flask_cors import CORS
 import os
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.llms import OpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAI
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 import json
@@ -13,11 +13,12 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 embeddings = OpenAIEmbeddings()
 vectordb = Chroma(persist_directory="chroma/", embedding_function=embeddings)
 qa = RetrievalQA.from_chain_type(
-    llm=OpenAI(), chain_type="stuff", retriever=vectordb.as_retriever())
+    llm=OpenAI(), chain_type="stuff", retriever=vectordb.as_retriever()
+)
 
 
 @app.route("/")
@@ -27,14 +28,14 @@ def index():
 
 @app.route("/response", methods=["POST"])
 def get_bot_response():
-    userText = request.json.get('message')
+    userText = request.json.get("message")
     print(userText)
-    result = qa.run(userText)
-    with open('temp.json', 'a') as w:
-        json.dump({'text': result}, w)
-    return {'text': result}
+    result = qa.invoke(userText)
+    with open("temp.json", "a") as w:
+        json.dump({"text": result}, w)
+    return {"text": result}
 
 
-if __name__ == '__main__':
-    p = int(os.getenv('PORT'))
-    app.run(host='0.0.0.0', port=p)
+if __name__ == "__main__":
+    p = int(os.getenv("PORT", 80))
+    app.run(host="0.0.0.0", port=p)
